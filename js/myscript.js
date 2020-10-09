94186b5aa00cf2c18c42c4fd1004e14ef75d2443
Christian Bodenwinkler <c.bodenwinkler@gmx.at>
2020-10-09 15:50:39 +0200

    Mostly Finished - Just needs some more CSS for looks

--- js/myscript.js
+++ js/myscript.js
@@ -2,18 +2,46 @@
 var movieData = JSON.parse(movies);
     // console.table(movieData);
 
+
 // For Loop to implement the data from JSON:
 for (let i = 0; i < movieData.length; i++) {
-    $("#movieContent").append(`<div id="movie${i}" class="imgPlusText col-lg-6 col-sm-12"></div>`);
+    $("#movieContent").append(`<div id="movie${i}" class="imgPlusText col-lg-6 col-md-12 col-sm-12"></div>`);
         $(`#movie${i}`).html(`
         <div class="borderME">
             <img id="img${i}" class="movieImg" src="${movieData[i].image}">
             <div class="movieSide">
                 <h3 class="movieName"> ${movieData[i].movieName} </h>
                 <p class="movieDesc"> ${movieData[i].description} </p>
-                <span class="likeBox"><button class="plus" type="plus">Like &#x1F44D; </button> <span>${movieData[i].likes}</span> </span>
+                <span id="${i}" class="likeBox"><button class="plus" type="plus">Like &#x1F44D; </button> <span id="givenLikes${i}" class="likes">${movieData[i].likes}</span> </span>
             </div>
         </div>
         `)
+};
 
+
+// Incrementing the Likes when clicking the Like button:
+$(document).ready(function () {
+    $(".plus").on("click", function() {
+        var index = $(this).parent().attr("id");
+        movieData[index].likes += 1;
+        $(this).parent().find(".likes").html(movieData[index].likes);
+    });
+});
+
+
+// Code to sort the Movies based on their current likes:
+var counter = 0;
+
+for (let i = 0; i < movieData.length; i++) {
+    // Fetching current likes from page:
+    $("#sortButton").on("click", function() {
+        var currentLikes = $(`#givenLikes${i}`).html();
+        // console.log(currentLikes);
+        
+        // Comparison to other Like Values:
+        if (currentLikes > counter) {
+            orderNr = currentLikes * (-1);
+            $(`#movie${i}`).css("order", orderNr);
+        }
+    });
 };
